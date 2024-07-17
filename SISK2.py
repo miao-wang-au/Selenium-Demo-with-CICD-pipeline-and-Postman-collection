78551ba6f96ccd912cfce49c6caaa18c2e7a2c38
miao-wang-au <40533399+miao-wang-au@users.noreply.github.com>
2024-07-17 23:40:41 +0200

    Add files via upload

--- SISK2.py
+++ SISK2.py
@@ -1,5 +1,6 @@
 import time
 from UserLogin import Login
+from Add3Items import AddThreeItems
 from datetime import datetime
 
 from selenium.webdriver import Keys
@@ -28,62 +29,68 @@ WebDriverWait(driver, 15).until(
     EC.presence_of_element_located((By.XPATH, "//tbody/tr[6]/td[4]"))
 )
 
-levelofstock = driver.find_element(By.XPATH, "//tbody/tr[6]/td[4]").text
+StockScan = driver.find_element(By.XPATH, "//tbody/tr[4]/td[4]").text  # Check No. of item 430005S before scan
+StockManual = driver.find_element(By.XPATH, "//tbody/tr[5]/td[4]").text  # Check No. of item 430004S before manual
+StockSearch = driver.find_element(By.XPATH, "//tbody/tr[6]/td[4]").text  # Check No. of item 430003S before search
 
-print(levelofstock)
+print("Number of stock for 430005S before being released: " + StockScan + "\n" +
+      "Number of stock for 430004S before being released: " + StockManual + "\n" +
+      "Number of stock for 430003S before being released: " + StockSearch)
 
 ###############################################################################################
 
 driver.find_element(By.XPATH, "//*[contains(text(), 'Home')]").click()
 
-# Click button 'Create Requisition Order'
-driver.find_element(By.XPATH, "//*[contains(text(), 'Create Requisition Order')]").click()
-
-# Click Select Cost Center dropdown
-WebDriverWait(driver, 5).until(
-     EC.presence_of_element_located((By.XPATH, "(//button[@title='Open'])[2]"))
+AddThreeItems(driver)
+# # Click button 'Create Requisition Order'
+# driver.find_element(By.XPATH, "//*[contains(text(), 'Create Requisition Order')]").click()
+#
+# # Click Select Cost Center dropdown
+# WebDriverWait(driver, 5).until(
+#     EC.presence_of_element_located((By.XPATH, "(//button[@title='Open'])[2]"))
+# )
+# driver.find_element(By.XPATH, "(//button[@title='Open'])[2]").click()
+#
+# driver.find_element(By.XPATH, "//*[contains(text(), 'code3')]").click()
+#
+# driver.find_element(By.ID, "consignment-invoice").click()
+#
+# driver.find_element(By.XPATH, "//button[@data-testid='tab-1']").click()
+#
+# driver.find_element(By.ID, "dropdown-field-id-0-0").send_keys("430003S")
+#
+# WebDriverWait(driver, 15).until(
+#     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), '430003S')]"))
+# )
+#
+# driver.find_element(By.XPATH, "//*[contains(text(), '430003S')]").click()
+#
+# driver.find_element(By.ID, "select-id-0-2").click()
+#
+# driver.find_element(By.XPATH, "//option[@data-testid='options-0-2-1']").click()
+#
+# driver.find_element(By.CSS_SELECTOR, "#text-field-id-0-4").send_keys(
+#     "{}".format("{}".format(datetime.now().strftime("%d-%m-%Y-%H%M%S"))))
+#
+# driver.find_element(By.CSS_SELECTOR, "#submitButton").click()
+#
+# driver.find_element(By.XPATH, "(//button[@type='button'])[14]").click()
+#
+# driver.find_element(By.XPATH, "//input[@type='password']").send_keys("1234")
+#
+# WebDriverWait(driver, 7).until(
+#     EC.presence_of_element_located((By.CSS_SELECTOR, "svg[data-testid='valid-pin-check']"))
+# )
+#
+# driver.find_element(By.XPATH, "(//button[@type='button'])[16]").click()
+#
+WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.XPATH, "(//div[@role='alert'])[1]"))
 )
-driver.find_element(By.XPATH, "(//button[@title='Open'])[2]").click()
 
-driver.find_element(By.XPATH, "//*[contains(text(), 'code3')]").click()
+RequisitionCreated = driver.find_element(By.XPATH, "(//div[@role='alert'])[1]").text
 
-driver.find_element(By.ID, "consignment-invoice").click()
-
-driver.find_element(By.XPATH, "//button[@data-testid='tab-1']").click()
-
-driver.find_element(By.ID, "dropdown-field-id-0-0").send_keys("430003S")
-
-WebDriverWait(driver, 15).until(
-     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), '430003S')]"))
-)
-
-driver.find_element(By.XPATH, "//*[contains(text(), '430003S')]").click()
-
-driver.find_element(By.ID, "select-id-0-2").click()
-
-driver.find_element(By.XPATH, "//option[@data-testid='options-0-2-1']").click()
-
-driver.find_element(By.CSS_SELECTOR, "#text-field-id-0-4").send_keys("{}".format("{}".format(datetime.now().strftime("%d-%m-%Y-%H%M%S"))))
-
-driver.find_element(By.CSS_SELECTOR, "#submitButton").click()
-
-driver.find_element(By.XPATH, "(//button[@type='button'])[14]").click()
-
-driver.find_element(By.XPATH, "//input[@type='password']").send_keys("1234")
-
-WebDriverWait(driver, 7).until(
-     EC.presence_of_element_located((By.CSS_SELECTOR, "svg[data-testid='valid-pin-check']"))
-)
-
-driver.find_element(By.XPATH, "(//button[@type='button'])[16]").click()
-
-WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.XPATH, "(//div[@role='alert'])[1]"))
-)
-
-requisitioncreated = driver.find_element(By.XPATH, "(//div[@role='alert'])[1]").text
-
-print(requisitioncreated)
+print(RequisitionCreated)
 
 driver.find_element(By.XPATH, "(//button[@title='Account settings'])[1]").click()
 driver.find_element(By.XPATH, "(//li[normalize-space()='Logout'])[1]").click()
@@ -93,25 +100,25 @@ driver.find_element(By.XPATH, "(//li[normalize-space()='Logout'])[1]").click()
 Login("Customer Service", driver)
 
 WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.LINK_TEXT, "Requisitions"))
+    EC.presence_of_element_located((By.LINK_TEXT, "Requisitions"))
 )
 
 driver.find_element(By.LINK_TEXT, "Requisitions").click()
 
 WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.XPATH, "(//button[@type='button'])[10]"))
+    EC.presence_of_element_located((By.XPATH, "(//button[@type='button'])[10]"))
 )
 
 driver.find_element(By.XPATH, "(//button[@type='button'])[10]").click()
 
 WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.XPATH, "(//button[@type='button'])[44]"))
+    EC.presence_of_element_located((By.XPATH, "(//button[@type='button'])[50]"))
 )
 
-driver.find_element(By.XPATH, "(//button[@type='button'])[44]").click()
+driver.find_element(By.XPATH, "(//button[@type='button'])[50]").click()
 
-WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.XPATH, "(//div[@class='MuiSnackbarContent-message'])[1]"))
+WebDriverWait(driver, 15).until(
+    EC.presence_of_element_located((By.XPATH, "(//div[@class='MuiSnackbarContent-message'])[1]"))
 )
 
 releasemessage = driver.find_element(By.XPATH, "(//div[@class='MuiSnackbarContent-message'])[1]").text
@@ -119,12 +126,12 @@ releasemessage = driver.find_element(By.XPATH, "(//div[@class='MuiSnackbarConten
 print(releasemessage)
 ##################################################################################################
 WebDriverWait(driver, 10).until(
-     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Stock Levels')]"))
+    EC.element_to_be_clickable((By.XPATH, "(//div[@class='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-9'])[2]"))
 )
 
-WebDriverWait(driver, 5)
+time.sleep(1)
 
-driver.find_element(By.XPATH, "//*[contains(text(), 'Stock Levels')]").click()
+driver.find_element(By.XPATH, "(//div[@class='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-9'])[2]").click()
 
 driver.find_element(By.LINK_TEXT, "Consignment").click()
 
@@ -132,15 +139,16 @@ WebDriverWait(driver, 15).until(
     EC.presence_of_element_located((By.XPATH, "//tbody/tr[6]/td[4]"))
 )
 
-levelofstockafter = driver.find_element(By.XPATH, "//tbody/tr[6]/td[4]").text
+StockScanReleased = driver.find_element(By.XPATH, "//tbody/tr[4]/td[4]").text  # Check No. of item 430005S by scan
+StockManualReleased = driver.find_element(By.XPATH, "//tbody/tr[5]/td[4]").text  # Check No. of item 430004S by manual
+StockSearchReleased = driver.find_element(By.XPATH, "//tbody/tr[6]/td[4]").text  # Check No. of item 430003S by search
 
-print(levelofstockafter)
+print("Number of stock for 430005S after being released: " + StockScanReleased + "\n"
+      "Number of stock for 430004S after being released: " + StockManualReleased + "\n"
+      "Number of stock for 430003S after being released: " + StockSearchReleased)
 
-assert "1" in "{}".format(int(levelofstock) - int(levelofstockafter))
+assert "1" in "{}".format(int(StockScan) - int(StockScanReleased))
+assert "0" in "{}".format(int(StockManual) - int(StockManualReleased))
+assert "1" in "{}".format(int(StockSearch) - int(StockSearchReleased))
 
-print("Success")
-
-#time.sleep(5)
-
-
-
+print("Regression Scenario 1 before CD message Successfully Executed")
