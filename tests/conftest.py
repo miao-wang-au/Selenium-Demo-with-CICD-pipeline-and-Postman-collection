32f38cf12e303337be7cdca65124473ee57c85ea
miao-wang-au <miaowang.au@gmail.com>
2025-12-04 19:56:31 +0100

    update cicd.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -17,16 +18,17 @@ def pytest_addoption(parser):
 def driver(request):
     browser = request.config.getoption("--browser")
 
-    options = Options()
-    options.add_argument('--headless')
-
     # Setup
     print(f"\nSetting up: {browser} driver")
     if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument('--headless')
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     elif browser == "firefox":
-        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 
     else:
         raise ValueError(f"Unsupported browser: {browser}")
